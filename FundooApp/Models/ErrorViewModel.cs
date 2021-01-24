53701af2eb4ff0c32c92a13afeb76657aa39764e
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2021-01-24 12:54:18 +0530

    [vidyadharhudge]Add:Welcome Message

---
+++ FundooApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FundooApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
