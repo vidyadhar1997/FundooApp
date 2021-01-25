674f8ef2aa992c4f1fcdb984562dacf2108d8829
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2021-01-25 11:59:01 +0530

    [vidyadharhudge]Add:Registration model and user context class

---
+++ FundooModel/Models/RegisterModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FundooModel.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Key]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ FundooRepository/Context/UserContext.cs
@@ -0,0 +1,17 @@
+﻿using FundooModel.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FundooRepository.Context
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<RegisterModel> RegisterModels {get; set;}
+    }
+}
